d6a58140aa49d1c3008f6424a1d817faa7714028
Genaro Salomone <genarosalomone@gmail.com>
2024-01-27 10:44:54 -0300

    Mensajes de conexión y desconexión en el cliente

--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -1,3 +1,19 @@
 
+//Referencias html
+const lblOnline = document.querySelector('#lblOnline');
+const lblOffline = document.querySelector('#lblOffline');
 
 const socket = io();
+
+socket.on('connect', () => {
+  console.log('Conectado');
+  lblOffline.style.display = 'none';
+  lblOnline.style.display = '';
+
+})
+
+socket.on('disconnect', () => {
+  console.log('Desconectado del servidor');
+  lblOnline.style.display = 'none';
+  lblOffline.style.display = '';
+})
