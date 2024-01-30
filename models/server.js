fceac8e41040d304f043e3f02eb2b419218810cc
Genaro Salomone <genarosalomone@gmail.com>
2024-01-30 09:18:42 -0300

    Retroalimentacion cliente - servidor

--- models/server.js
+++ models/server.js
@@ -34,11 +34,13 @@ class Server {
 
   sockets() {
     this.io.on('connection', socket => {
-      console.log("Cliente conectado", socket.id);
-      socket.on('disconnect', () => console.log("Cliente desconectado", socket.id));
 
-      socket.on('enviar-mensaje', (payload) => {
-        this.io.emit('enviar-mensaje', payload)
+      // socket.on('disconnect', () => console.log("Cliente desconectado", socket.id));
+
+      socket.on('enviar-mensaje', (payload, callback) => {
+        const id = 123456789;
+        callback(id);
+        // this.io.emit('enviar-mensaje', payload)
       })
     })
   }
